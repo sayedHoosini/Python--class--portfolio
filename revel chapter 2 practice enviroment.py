e4c682c21fc33e9de1f6c0828677776afe53f5d2
Hashim Sayed Hoosini <babak201070@gmail.com>
2026-02-08 07:55:07 -0600

    finding area of a circle

---
+++ revel chapter 2 practice enviroment.py
@@ -0,0 +1,18 @@
+# This is the revel practice enviroment for chapter 2
+# area of _circle.py
+# This is first method
+import math 
+# give us math.pi
+# 1 ) input
+radius = float(input("Enter the radius of the circle: "))
+# compute 
+area = math.pi*(radius**2)
+# output ( two decimal places)
+print(f"Area = {area:.2f}")
+#
+# 
+# This is second method
+# this is the area of the circle 
+radius =20
+area=radius*radius* 3.14159
+print("The area for the circle of radius", radius, "is", area)
