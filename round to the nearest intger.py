664d1c5a10a9b92becc53438da9e380205414559
Hashim Sayed Hoosini <babak201070@gmail.com>
2026-02-07 22:10:56 -0600

    nearest integer, rounded to 3 decimal places, rounded to 1 decimal places

---
+++ round to the nearest intger.py
@@ -0,0 +1,9 @@
+# round(3.5) return the nearest integer value
+round(3.7)
+print(round(3.5))
+# round((3.1456,3) return the value of 3.1456 rounded to 3 decimal places
+round(3.1456,3)
+print(round(3.1456,3))
+# round(3.85,1) return the value of 3.85 rounded to 1 decimal place
+round(3.85,1)
+print(round(3.85,1))
