2ef07325594a4aa2cf49a0076cb94677333fa982
Hashim Sayed Hoosini <babak201070@gmail.com>
2026-02-07 22:05:56 -0600

    power.py

--- 2the power 3.py
+++ 2the power 3.py
@@ -1,3 +1,6 @@
 # pow(2,3) return the value of 2 raised to the power of 3
 pow(2,3)
 print(pow(2,3))
+# pow(2.5,3.5) return the value of 2.5 raised to the power of 3.5
+pow(2.5,3.5)
+print(pow(2.5,3.5))
