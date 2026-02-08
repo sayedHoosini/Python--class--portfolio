9efd05193abcec0560a40f792a3d73e57f2fa286
Hashim Sayed Hoosini <babak201070@gmail.com>
2026-02-07 22:18:25 -0600

    Add files via upload

---
+++ square root, decimal, fabs,ceil,floor.py
@@ -0,0 +1,21 @@
+# square root of 25
+import math
+math.sqrt(25)
+print(math.sqrt(25))
+
+import decimal
+decimal.Decimal(3.85)
+print(decimal.Decimal(3.85))
+
+
+# fabs (x) return the absolute value of x
+math.fabs(-3.5)
+print(math.fabs(-3.5))
+
+# ceil (x) return the smallest integer greater than or equal to x
+math.ceil(3.5)
+print(math.ceil(3.5))
+
+# floor (x) return the largest integer less than or equal to x
+math.floor(3.5)
+print(math.floor(3.5))
